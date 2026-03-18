afabb86695aa04a31f1dd77be7086870f3c1f9d1
Priyanshu Vora <92289465+privo211@users.noreply.github.com>
2026-03-18 15:12:50 -0400

    Nunhems: parse euro float fix 2.

--- vendor_extractors/nunhems.py
+++ vendor_extractors/nunhems.py
@@ -526,12 +526,8 @@ AZURE_KEY      = os.getenv("AZURE_KEY")
 def parse_euro_float(value):
   """
   Swaps all commas with dots and all dots with commas in a string representation of a float.
-
-  Args:
-    value: The input number, as a string or a float/int which will be converted to string.
-
-  Returns:
-    The string with swapped separators.
+  Args: value: The input number, as a string or a float/int which will be converted to string.
+  Returns: The float value with swapped separators.
   """
   # Convert the input to a string first, in case it's a float/int
   s = str(value)
@@ -544,8 +540,9 @@ def parse_euro_float(value):
 
   # Apply the translation
   swapped_s = s.translate(translation_table)
+  fvalue = float(swapped_s)
 
-  return swapped_s
+  return fvalue
 
 # def parse_euro_float(s: str) -> float:
 #     """
