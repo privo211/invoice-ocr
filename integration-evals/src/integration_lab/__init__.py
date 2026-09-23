45ad17db08742fd18c16c98f537362830cbc5edb
Priyanshu Vora <priyanshu.vora211@gmail.com>
2026-09-23 17:13:09 -0400

    Remove mistakenly added integration-evals project from invoice-ocr

--- integration-evals/src/integration_lab/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reliable API integration patterns, implemented against JSONPlaceholder."""
-
-from .client import IntegrationClient
-from .models import Post, User
-
-__all__ = ["IntegrationClient", "Post", "User"]
